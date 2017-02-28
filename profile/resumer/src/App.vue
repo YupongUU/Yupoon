ca410d8ddbdbf259d9b65048be999ecac7b38c35
YupongUU <cnfeat@gmail.com>
2017-02-28 23:52:51 +0800

    create Resumer

---
+++ profile/resumer/src/App.vue
@@ -0,0 +1,80 @@
+<!-- template:内容 1.有且仅有一个儿子标签 -->
+<template>
+  <div class="page">
+    <header>
+      <Topbar />
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+
+    </main>
+  </div>
+</template>
+
+<script>
+  import 'normalize.css/normalize.css'
+  import './assets/reset.css'
+  import Topbar from './components/Topbar'
+  import ResumeEditor from './components/ResumeEditor'
+  import ResumePreview from './components/ResumePreview'
+  import icons from './assets/icons'
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        text: '你好'
+      }
+    },
+    components: {
+      Topbar,
+      ResumeEditor,
+      ResumePreview
+    },
+    created() {
+      document.body.insertAdjacentHTML('afterbegin', icons)
+    }
+  }
+
+</script>
+<style lang="scss">
+  .page {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    background: #eaebec;
+    >main {
+      flex-grow: 1;
+      min-width: 900px;
+      max-width: 1440px;
+      margin-top: 16px;
+      margin-bottom: 16px;
+      display: flex;
+      justify-content: space-between;
+      padding: 0 16px;
+      width: 100%;
+      align-self: center;
+    }
+  }
+  
+  #resumeEditor {
+    width: 35%;
+    background: #444;
+  }
+  
+  #resumePreview {
+    flex-grow: 1;
+    margin-left: 16px;
+    background: #777;
+  }
+  
+  svg.icon {
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size: 16px;
+  }
+
+</style>
---
+++ profile/resumer/src/components/ResumePreview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="resumePreview">
+    I am resumePreview
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ResumePreview'
+}
+</script>
+
+<style lang="scss">
+  #resumePreview{
+    color: blue;
+    background: #fff;
+    box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.25)
+  }
+</style>
